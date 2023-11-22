31ee13d1897e54823b5b31c98cbdfa7232938bc7
Lodovico Ardissao <lodoviko@gmail.com>
2023-11-22 20:18:29 -0300

    20231122a - Aula de desafios

---
+++ src/main/java/com/algaworks/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_item_pedido")
+public class ItemPedido {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id",nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id",nullable = false)
+    private Produto produto;
+}
