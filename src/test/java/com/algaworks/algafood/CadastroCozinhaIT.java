f5724b7eb61dc95006087932f2b9c5117361358f
Lodovico Ardissao <lodoviko@gmail.com>
2023-12-07 18:53:08 -0300

    20231205a - Testes de api

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,26 +1,46 @@
-package com.algaworks.algafoodapi;
+package com.algaworks.algafood;
 
-import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = AlgafoodApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@BeforeEach   // Preparação: Esse método executa antes de cada um dos métodos de teste.
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+
+		databaseCleaner.clearTables();
+		this.prepararDados();
+	}
+
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
 		RestAssured.given()
-					.basePath("/cozinhas")
-					.port(port)
 					.accept(ContentType.JSON)
 				.when()
 					.get()
@@ -29,6 +49,39 @@ class CadastroCozinhaIT {
 
 	}
 
+	@Test
+	public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
+
+		RestAssured.given()
+					.accept(ContentType.JSON)
+				.when()
+					.get()
+				.then()
+					.body("", Matchers.hasSize(2))
+					.body("nome", Matchers.hasItems("Americana", "Tailandesa"));
+	}
+
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+				.body("{\"nome\":\"Chinesa\"}")
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+			.when()
+				.post()
+			.then()
+				.statusCode(HttpStatus.CREATED.value());
+	}
+
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
+	}
 
 //
 //	@Autowired
