79c71dfffdb609ae09ab4e2c3f760bc2acae9e99
Lodovico Ardissao <lodoviko@gmail.com>
2023-12-15 19:59:02 -0300

    20231215b - Atualizacao

---
+++ src/main/java/com/algaworks/algafood/api/dto/model/RestauranteModelDTO.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.api.dto.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class RestauranteModelDTO{
+        Long id;
+        String nome;
+        BigDecimal taxaFrete;
+        CozinhaModelDTO cozinha;
+}
