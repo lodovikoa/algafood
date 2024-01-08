228e8d837a27883420354e931cf751abd750572b
Lodovico Ardissao <lodoviko@gmail.com>
2024-01-08 20:50:58 -0300

    20240108b - Projecoes

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -10,9 +10,14 @@ import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.model.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.exception.NegocioException;
 import com.algaworks.algafood.domain.model.service.PedidoEmissaoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import jakarta.validation.Valid;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,10 +37,27 @@ public class PedidoController {
     private PedidoInputDTODisassembler pedidoInputDTODisassembler;
 
     @GetMapping
-    public List<PedidoResumoModelDTO> listar() {
-        var todosPedidos = pedidoEmissaoService.findAll();
-        return pedidoResumoModelDTOAssembler.toCollectionModel(todosPedidos);
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
+        var pedidosModel = pedidoResumoModelDTOAssembler.toCollectionModel(pedidoEmissaoService.findAll());
+        var pedidosWrapper = new MappingJacksonValue(pedidosModel);
+
+        var filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if(StringUtils.isNotBlank(campos)) {
+            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
     }
+
+//    @GetMapping
+//    public List<PedidoResumoModelDTO> listar() {
+//        var todosPedidos = pedidoEmissaoService.findAll();
+//        return pedidoResumoModelDTOAssembler.toCollectionModel(todosPedidos);
+//    }
 
     @GetMapping("/{pedidoCodigo}")
     public PedidoModelDTO buscarPedido(@PathVariable String pedidoCodigo) {
