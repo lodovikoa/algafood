0f231ad7145994739e860ee2f57c85b92fd58e9e
Lodovico Ardissao <lodoviko@gmail.com>
2024-01-16 20:57:52 -0300

    20240116e - Aula 13.15 - Estatisticas

--- src/main/java/com/algaworks/algafood/infrastructue/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructue/service/VendaQueryServiceImpl.java
@@ -20,12 +20,13 @@ public class VendaQueryServiceImpl implements VendaQueryService {
     @Autowired
     private EntityManager manager;
     @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
        var builder = manager.getCriteriaBuilder();
        var query = builder.createQuery(VendaDiaria.class);
        var root = query.from(Pedido.class);
 
-       var funcionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
+       var functionConvertTzDataCriacao = builder.function("convert_tz", Date.class, root.get("dataCriacao"), builder.literal("+00:00"), builder.literal(timeOffset));
+       var funcionDateDataCriacao = builder.function("date", Date.class, functionConvertTzDataCriacao);
 
        var selection = builder.construct(VendaDiaria.class,
                funcionDateDataCriacao,
