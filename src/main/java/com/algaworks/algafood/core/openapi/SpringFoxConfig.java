a8a692f3ab84957b43a1bdee0d31e8d463cd9b48
Lodovico Ardissao <lodoviko@gmail.com>
2024-02-07 21:11:46 -0300

    20240207a - Swagger

---
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.core.openapi;
+
+//@Configuration
+//@EnableSwagger2
+public class SpringFoxConfig {
+
+//    @Bean
+//    public Docket apiDocket() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .build();
+//    }
+}
