4d8c3205b8a3e8a75a512cb51856caa245bcb34d
Lodovico Ardissao <lodoviko@gmail.com>
2024-02-16 08:31:49 -0300

    20240216a - aula 19.18 hateoas links com variaveis

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelDTOAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelDTOAssembler.java
@@ -5,6 +5,10 @@ import com.algaworks.algafood.api.dto.model.PedidoModelDTO;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.MvcLink;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -28,7 +32,18 @@ public class PedidoModelDTOAssembler extends RepresentationModelAssemblerSupport
         var pedidoModelDTO = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModelDTO);
 
-        pedidoModelDTO.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+        var pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
+        );
+
+        var pedidosUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
+
+        pedidoModelDTO.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
+
+        // pedidoModelDTO.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+
         pedidoModelDTO.getRestaurante().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())).withSelfRel());
         pedidoModelDTO.getCliente().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(UsuarioController.class).buscarPorId(pedido.getCliente().getId())).withSelfRel());
 
