f8d2f22750261df92802a125f95d4ffd238a5ed4
Lodovico Ardissao <lodoviko@gmail.com>
2024-02-28 10:41:38 -0300

    20240228a - aula implementando seguranca

---
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    //@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/v1/cozinhas/**").permitAll()
+
+                        .anyRequest().authenticated()
+                )
+                .build();
+
+
+    }
+
+}
