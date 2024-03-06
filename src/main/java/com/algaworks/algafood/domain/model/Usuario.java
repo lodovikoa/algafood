70d84e2799fc8246ac610cd2f0825f2f3b4583cb
Lodovico Ardissao <lodoviko@gmail.com>
2024-03-06 19:40:27 -0300

    20240306a - aula implementando seguranca

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -40,14 +40,6 @@ public class Usuario {
     @JoinTable(name = "tb_usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
     private Set<Grupo> grupos = new HashSet<>();
 
-//    public boolean senhaCoincideCom(String senha) {
-//        return getSenha().equals(senha);
-//    }
-//
-//    public boolean senhaNaoCoincideCom(String senha) {
-//        return !senhaCoincideCom(senha);
-//    }
-
     public boolean removerGrupo(Grupo grupo) {
         return this.grupos.remove(grupo);
     }
